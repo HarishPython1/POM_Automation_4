be10ab229c1e057c90d12b1a3cc2e88f80aae4fe
harishpython1 <harish.python.com@gmail.com>
2019-07-02 09:34:52 +0530

    Added Login Page Automation Demo

--- pages/LocatorGeneric.py
+++ pages/LocatorGeneric.py
@@ -1,8 +1,9 @@
+from selenium.common.exceptions import *
 class LocatorGeneric:
     def __init__(self, driver):
         self.driver = driver
-
     def locator(self, loc_type, locator_val):
+        try:
             if loc_type == "name":
                 ele = self.driver.find_element_by_name(locator_val)
             elif loc_type == "id":
@@ -12,4 +13,30 @@ class LocatorGeneric:
             elif loc_type == "tagname":
                 ele = self.driver.find_element_by_tag_name(locator_val)
             return ele
+        except NoSuchElementException as e:
+            print(e)
+        except ElementNotInteractableException as e:
+            print(e)
+        except WebDriverException as e:
+            print(e)
+        except NoSuchWindowException as e:
+            print(e)
+        except UnexpectedTagNameException as e:
+            print(e)
+        except ElementClickInterceptedException as e:
+            print(e)
+        except SessionNotCreatedException as e:
+            print(e)
+        except StaleElementReferenceException as e:
+            print(e)
+        except Exception as e:
+            print(e)
+
+
+
+
+
+
+
+
 
